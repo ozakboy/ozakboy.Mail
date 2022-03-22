5111f9354f9d51179996d5ceb70572eefc7bfaf3
ozakboy <ozakboy@gmail.com>
2022-03-22 14:19:30 +0800

    新增 API 測試專案

---
+++ Ozakboy.Mail/Ozakboy.Mail.WebApiTest/Controllers/SendEmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace Ozakboy.Mail.WebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+        private IMail _mail;
+        public SendEmailController(IMail mail)
+        {
+            _mail = mail;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<MailInfo> mailInfos = new List<MailInfo>();
+            List<MailInfo> CCInfos = new List<MailInfo>();
+            List<AttachmentsInfo> AttachmentsInfos = new List<AttachmentsInfo>();
+            mailInfos.Add(new MailInfo("你的信箱"));
+            CCInfos.Add(new MailInfo("你的信箱"));
+
+            FileStream fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json", FileMode.Open);
+            AttachmentsInfos.Add(new AttachmentsInfo
+            {
+                FileName = "測試檔案.json",
+                FileType = MediaTypeNames.Application.Json,
+                FileStream = fileStream
+            });
+            _mail.SendMail(mailInfos, CCInfos, AttachmentsInfos, "測試寄信標題", "測試寄信功能", false);
+            return Ok();
+        }
+
+    }
+}
---
+++ Ozakboy.Mail/Ozakboy.Mail.WebApiTest/Program.cs
@@ -0,0 +1,32 @@
+using Ozakboy.Mail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region ¬Û¨Ìª`¤J
+builder.Services.AddScoped<IMail, Mail>();
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
